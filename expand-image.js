29920cb989a6a22393233bccdd4197c08ca38834
nnatsumy <natalia.padua@analogica.com.br>
2018-10-22 10:41:56 -0300

    expansão de imagens

--- expand-image.js
+++ expand-image.js
@@ -1,19 +1,31 @@
-function expand(img){
+function expand(e){
 		console.log('aqui');
-        document.getElementById('imgmodal').src = img.src;
-        document.getElementById('modal-background').fadeIn();
-        document.getElementById('modal-background').click(function(){
-            closeModal();
-         });
-
+		let el = e.currentTarget;
+		document.getElementById('imgmodal').src = el.src;
+		let img = document.getElementById('imgclose');
+		img.style.opacity = 1.0;
+        //document.getElementById('imgmodal').src = img.src;
 }
 
-function closeModal(){
-  $('#modal-background').fadeOut();
+function close(e){
+		let el = e.currentTarget;
+		document.getElementById('imgmodal').src = '';
+		el.style.opacity = 0;
 }
 
-let mc = document.getElementById('modal-content');
-let mb = document.getElementById('modal-background');
-let img = document.getElementById('img1');
-img.addEventListener('click',expand(img,mc,mb));
 
+let img1 = document.getElementById('img1');
+let img2 = document.getElementById('img2');
+let img3 = document.getElementById('img3');
+let img4 = document.getElementById('img4');
+let img5 = document.getElementById('img5');
+
+let closeimg = document.getElementById('imgclose');
+
+img1.addEventListener('click',expand);
+img2.addEventListener('click',expand);
+img3.addEventListener('click',expand);
+img4.addEventListener('click',expand);
+img5.addEventListener('click',expand);
+
+closeimg.addEventListener('click',close);
